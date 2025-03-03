f97d5dc6c15e0604f26ee6efe3337079235b923c
dfxhub <4047760+dfxhub@users.noreply.github.com>
2025-03-03 21:38:50 +0400

    Add files via upload

--- fw/asmprocs.h
+++ fw/asmprocs.h
@@ -10,13 +10,13 @@
 #define C4PUS (CYCLES_PER_US/4)
 #define DVUS(x) (C4PUS*x)
 
-extern uint16_t swap16(uint16_t value);
+//extern uint16_t swap16(uint16_t value);
 extern void _delay_us_asm(uint16_t us);
-extern void as_i2c_start(void);
+//extern void as_i2c_start(void);
 
-extern uint8_t _rand_lfsr(uint8_t sr);
-extern uint8_t _rand_marv(uint8_t sr);
-extern uint8_t _rand_galo(uint8_t sr);
+//extern uint8_t _rand_lfsr(uint8_t sr);
+//extern uint8_t _rand_marv(uint8_t sr);
+//extern uint8_t _rand_galo(uint8_t sr);
 
 #define _delay_us(x) _delay_us_asm(DVUS(x))
 
